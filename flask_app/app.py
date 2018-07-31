2e40c92a515c40262687a8b82e91f549b0c74b38
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-07-31 21:00:48 +0000

    change the port to preview in cloud9

--- flask_app/app.py
+++ flask_app/app.py
@@ -3,7 +3,11 @@ from flask import Flask
 print('running flask_app')
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret'
 @app.route('/')
 def hello_world():
-  return "hello world!"
+  return "hello world! python3"
 
+
+if __name__ == "__main__":
+  app.run(host='0.0.0.0', port=8080, debug=True)
