32f20bb68fac5ff8a177b8f91524b60e5b3331d1
Ramsey Darling <ramseydarling@gmail.com>
2018-08-02 20:34:10 +0000

    iterating through python object collection

--- python_class/person.py
+++ python_class/person.py
@@ -10,6 +10,10 @@ class Person:
 
 calvin = Person("calvin", "settachatgul")
 ramsey = Person("ramsey", "darling")
+person3 = Person("person3", "person3last")
+person4 = Person("person4", "person4last")
+
+people = [calvin, ramsey, person3, person4]
 
 print("describe Person new a person has a first name and last name")
 print(calvin.firstname == "calvin")
@@ -20,3 +24,16 @@ print(ramsey.lastname == "darling")
 print("describe Person say_hello expect a person to say hello and return 'hello my name is firstname lastname'")
 print(calvin.say_hello() == 'hello my name is calvin settachatgul')
 print(ramsey.say_hello() == 'hello my name is ramsey darling')
+
+# iterate through these people and print the first name and last name
+# execute the say_hello function
+# arr =[];
+# for( var i=0; i< arr.length; i++){
+  
+# }
+
+for person in people:
+  print (person.firstname)
+  print (person.lastname)
+  print(person.say_hello())
+  
