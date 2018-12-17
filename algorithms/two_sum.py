560a529828d00f5b90efacadd8f332d39356edbb
Ramsey Darling <ramseydarling@gmail.com>
2018-12-16 22:53:12 -0800

    Update two_sum.py

--- algorithms/two_sum.py
+++ algorithms/two_sum.py
@@ -1,6 +1,7 @@
+#https://leetcode.com/problems/two-sum/
 
 
-Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+!!!Given an array of integers, return indices of the two numbers such that they add up to a specific target.
 
 You may assume that each input would have exactly one solution, and you may not use the same element twice.
 
@@ -9,4 +10,4 @@ Example:
 Given nums = [2, 7, 11, 15], target = 9,
 
 Because nums[0] + nums[1] = 2 + 7 = 9,
-return [0, 1].
+return [0, 1].!!!
