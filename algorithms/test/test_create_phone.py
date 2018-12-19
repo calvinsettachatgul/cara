3cd96ee8a3a84847ab5b5605a26eb532d90d3314
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-12-18 22:06:13 -0800

    feature-11: remove some comments

--- algorithms/test/test_create_phone.py
+++ algorithms/test/test_create_phone.py
@@ -1,9 +1,5 @@
 from create_phone import create_phone_number
 
-# sum up all the numbers from 1 to input_num
-# if its less than 1 then return 0
-
-
 def test_create_phone():
     '''create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890" '''
     assert create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) == "(123) 456-7890"
