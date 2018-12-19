665374b85829992a7745ee4a31d65eb199d504f6
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2018-12-18 22:05:30 -0800

    feature-11: remove some comments

--- algorithms/test/test_example.py
+++ algorithms/test/test_example.py
@@ -1,9 +1,5 @@
 from example import example
 
-# sum up all the numbers from 1 to input_num
-# if its less than 1 then return 0
-
-
 def test_example():
     '''test output of example'''
     assert example() == 'this is just an example'
