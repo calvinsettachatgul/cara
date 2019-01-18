91c960a3af40e9ea5938a20ea52fc7bfef9f25e7
Ramsey Darling <ramseydarling@gmail.com>
2019-01-18 01:55:17 -0800

    first commit to local repo using git bash!

--- algorithms/create_phone.py
+++ algorithms/create_phone.py
@@ -1,15 +1,21 @@
 # https://www.codewars.com/kata/create-phone-number/train/python
 
 '''
-Write a function that accepts an array of 10 integers (between 0 and 9), 
+Write a function that accepts an array of 10 integers (between 0 and 9),
 that returns a string of those numbers in the form of a phone number.
 
 Example:
 create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) # => returns "(123) 456-7890"
-The returned format must be correct in order to complete this challenge. 
+The returned format must be correct in order to complete this challenge.
 Don't forget the space after the closing parentheses!
 '''
 
 # TODO: create a test file
 def create_phone_number(num_list):
-    return num_list
+    num_list.insert(0,'(')
+    num_list.insert(4,')')
+    num_list.insert(5,' ')
+    num_list.insert(9,'-')
+    delimiter = ''
+    str_num_list = delimiter.join(map(str, num_list))
+    return str_num_list
