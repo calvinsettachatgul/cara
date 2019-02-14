d2f157c10976275cc06d14ca7439f01dd0a2c183
Calvin Settachatgul <calvin.settachatgul@gmail.com>
2019-02-14 06:21:41 +0000

    feature-15: some closure

---
+++ closure/closure_example.py
@@ -0,0 +1,52 @@
+# closure example
+
+# def make_adder_of(count):
+#     def adder(x):
+#         print("before {}".format(count))
+#         return count + x
+#         print("after {}".format(count))
+#     return add_one
+    
+# add1 = make_adder_of(1)
+# add2 = make_adder_of(2)
+# add5 = make_adder_of(5)
+
+# print(add5(4))
+
+# my_adder()
+# my_adder()
+# my_adder()
+
+def print_msg(msg):
+# This is the outer enclosing function
+
+    def printer():
+# This is the nested function
+        # msg = msg + " and goodbye"
+        print(msg)
+
+    printer()
+
+# We execute the function
+# Output: Hello
+print_msg("Hello")
+
+def make_multiplier_of(n):
+    def multiplier(x):
+        return x * n
+    return multiplier
+
+# Multiplier of 3
+times3 = make_multiplier_of(3)
+
+# Multiplier of 5
+times5 = make_multiplier_of(5)
+
+# Output: 27
+print(times3(9))
+
+# Output: 15
+print(times5(3))
+
+# Output: 30
+print(times5(times3(2)))
